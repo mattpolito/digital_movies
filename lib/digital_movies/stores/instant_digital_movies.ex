d194b8d944db1b0ddef5341d05b3f54bb16493d5
Matt Polito <mattpolito@users.noreply.github.com>
2021-07-28 22:40:40 -0400

    Categorize type for InstantDigitalMovies

--- lib/digital_movies/stores/instant_digital_movies.ex
+++ lib/digital_movies/stores/instant_digital_movies.ex
@@ -28,6 +28,7 @@ defmodule DigitalMovies.Stores.InstantDigitalMovies do
 
     Regex.named_captures(regex, title)
     |> extract_title_and_type(title)
+    |> categorize_type()
   end
 
   def parse_product_url(product) do
