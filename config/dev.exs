b3c4544fb7eb79622a71e66a3234891ae421c9c9
Matt Polito <mattpolito@users.noreply.github.com>
2021-09-21 14:14:59 -0400

    Enable ssl for repo connection

--- config/dev.exs
+++ config/dev.exs
@@ -7,7 +7,8 @@ config :digital_movies, DigitalMovies.Repo,
   database: "digital_movies_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
-  pool_size: 10
+  pool_size: 10,
+  ssl: true
 
 # For development, we disable any cache and enable
 # debugging and code reloading.
