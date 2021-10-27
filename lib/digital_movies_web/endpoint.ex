572244391c8e94a71a28ea0fd57c658c2e1a036e
Matt Polito <mattpolito@users.noreply.github.com>
2021-10-27 09:40:46 -0400

    Add phoenix static buildpack config

--- lib/digital_movies_web/endpoint.ex
+++ lib/digital_movies_web/endpoint.ex
@@ -11,10 +11,13 @@ defmodule DigitalMoviesWeb.Endpoint do
   ]
 
   socket "/socket", DigitalMoviesWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
-  socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
+  socket "/live", Phoenix.LiveView.Socket, websocket: [
+    connect_info: [session: @session_options],
+    timeout: 45_000
+  ]
 
   # Serve at "/" the static files from "priv/static" directory.
   #
